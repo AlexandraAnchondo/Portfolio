937db4200d24b80fcc7cfd3b7a400384e53261db
AgentSeven707 <alexandra.anchondo@uabc.edu.mx>
2024-03-01 14:01:59 -0800

    Add new files and styles for portfolio components

---
+++ portfolio/src/components/about/AboutBox.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const AboutBox = () => {
+    return (
+        <div className="about__boxes grid">
+            <div className="about__box">
+                <i className="about__icon icon-fire"></i>
+                
+                <div>
+                    <h3 className="about__title">4</h3>
+                    <span className="about__subtitle">Projects completed</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <i className="about__icon icon-book-open"></i>
+                
+                <div>
+                    <h3 className="about__title">3</h3>
+                    <span className="about__subtitle">Courses/certifications taken</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <i className="about__icon icon-hourglass"></i>
+                
+                <div>
+                    <h3 className="about__title">1</h3>
+                    <span className="about__subtitle">Years of experience</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutBox;
