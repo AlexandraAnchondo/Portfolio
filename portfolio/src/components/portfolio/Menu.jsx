82a1efc50f6c5be6e044f59123eaeb7c6e790b64
AgentSeven707 <alexandra.anchondo@uabc.edu.mx>
2024-03-26 21:22:53 -0700

    Update images and styles in About and Home components

--- portfolio/src/components/portfolio/Menu.jsx
+++ portfolio/src/components/portfolio/Menu.jsx
@@ -1,4 +1,4 @@
-import Work1 from "../../assets/work-1.svg";
+import Work1 from "../../assets/portfolio.png";
 import Work2 from "../../assets/work-2.svg";
 import Work3 from "../../assets/work-3.svg";
 import Work4 from "../../assets/work-4.svg";
@@ -9,39 +9,45 @@ const Menu = [
     {
         id: 1,
         image: Work1,
-        title: "Project Management Illustration",
+        title: "Portfolio Website",
         category: "Design",
+        url: 'https://alexandra-portfolio-cf21c.web.app/#home'
     },
-    {
-        id: 2,
-        image: Work2,
-        title: "Guest App Walkthrough Screens",
-        category: "Art",
-    },
-    {
-        id: 3,
-        image: Work3,
-        title: "Delivery App Wireframe",
-        category: "Branding",
-    },
-    {
-        id: 4,
-        image: Work4,
-        title: "Onboarding Motivation",
-        category: "Design",
-    },
-    {
-        id: 5,
-        image: Work5,
-        title: "iMac Mockup Design",
-        category: "Creative",
-    },
-    {
-        id: 6,
-        image: Work6,
-        title: "Game Store App Concept",
-        category: "Art",
-    },
+    // {
+    //     id: 2,
+    //     image: Work2,
+    //     title: "Guest App Walkthrough Screens",
+    //     category: "Art",
+    //     url: 'https://alexandra-portfolio-cf21c.web.app/#home'
+    // },
+    // {
+    //     id: 3,
+    //     image: Work3,
+    //     title: "Delivery App Wireframe",
+    //     category: "Branding",
+    //     url: 'https://alexandra-portfolio-cf21c.web.app/#home'
+    // },
+    // {
+    //     id: 4,
+    //     image: Work4,
+    //     title: "Onboarding Motivation",
+    //     category: "Design",
+    //     url: 'https://alexandra-portfolio-cf21c.web.app/#home'
+    // },
+    // {
+    //     id: 5,
+    //     image: Work5,
+    //     title: "iMac Mockup Design",
+    //     category: "Creative",
+    //     url: 'https://alexandra-portfolio-cf21c.web.app/#home'
+    // },
+    // {
+    //     id: 6,
+    //     image: Work6,
+    //     title: "Game Store App Concept",
+    //     category: "Art",
+    //     url: 'https://alexandra-portfolio-cf21c.web.app/#home'
+    // },
 ];
 
 export default Menu;
