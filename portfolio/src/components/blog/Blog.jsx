9192147b28aaf5c80dfa980f9e1e1ec7486debb1
AlexandraAnchondo <alexandra.anchondo.17@gmail.com>
2025-06-26 11:38:15 -0700

    New cards for blog, add back to the top button and update info

--- portfolio/src/components/blog/Blog.jsx
+++ portfolio/src/components/blog/Blog.jsx
@@ -4,6 +4,7 @@ import Image1 from "../../assets/blog-1.svg";
 import Image2 from "../../assets/blog-2.svg";
 import Image3 from "../../assets/blog-3.svg";
 import Image4 from "../../assets/blog-4.svg";
+import Card from "./InterestCard";
 
 const Blog = () => {
     return (
@@ -11,41 +12,10 @@ const Blog = () => {
             <h2 className="section__title">Interests</h2>
 
             <div className="blog__container grid">
-                <div className="blog__card">
-                    <div className="blog__thumb">
-                        <img src={Image1} alt="" className="blog__img"/>
-                    </div>
-                    <div className="blog__details">
-                        <h3 className="blog__title">Traditional and digital drawing</h3>
-                    </div>
-                </div>
-
-                <div className="blog__card">
-                    <div className="blog__thumb">
-                        <img src={Image2} alt="" className="blog__img"/>
-                    </div>
-                    <div className="blog__details">
-                        <h3 className="blog__title">Read and write novels</h3>
-                    </div>
-                </div>
-
-                <div className="blog__card">
-                    <div className="blog__thumb">
-                        <img src={Image3} alt="" className="blog__img"/>
-                    </div>
-                    <div className="blog__details">
-                        <h3 className="blog__title">Go to the gym</h3>
-                    </div>
-                </div>
-
-                <div className="blog__card">
-                    <div className="blog__thumb">
-                        <img src={Image4} alt="" className="blog__img"/>
-                    </div>
-                    <div className="blog__details">
-                        <h3 className="blog__title">Play videogames</h3>
-                    </div>
-                </div>
+                <Card classNanme="blog__card" image={Image1} description={"Traditional and digital drawing"} color={"#3097e1" }/>
+                <Card classNanme="blog__card" image={Image2} description={"Read and write novels"} color={"#ff9966" }/>
+                <Card classNanme="blog__card" image={Image3} description={"Go to the gym"} color={"#ff3e92" }/>
+                <Card classNanme="blog__card" image={Image4} description={"Play videogames"} color={"#2c3b46" }/>
             </div>
         </section>
     );
